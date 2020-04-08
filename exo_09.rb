cb3a2ef809adf12301377203027cf2729b0674c6
ALTERNATIF69 <ALTERNATIF69@users.noreply.github.com>
2020-04-08 03:15:05 +0200

    First commit

---
+++ exo_09.rb
@@ -0,0 +1,15 @@
+puts "Bonjour, c'est quoi ton prénom?"
+print ">"
+user_name = gets.chomp
+puts user_name
+
+puts "Quel est ton nom ?"
+userLastname = gets.chomp
+puts "Bonjour #{userLastname} !"
+
+
+-------- indication -------
+le prénom de l'utilisateur, 
+qui lui demande après son nom de famille
+et qui salue l'utilisateur avec "Bonjour, prénom nom !"
+= end
